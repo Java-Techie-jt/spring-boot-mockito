97ee4bbe5ffe0265361765be8069f55cbc115353
Fabio jr <fabiojcbweb@gmail.com>
2022-03-13 18:26:08 -0300

    Configuração maven, docker, vscode

--- src/test/java/com/javatechie/spring/mockito/api/SpringBootMockitoApplicationTests.java
+++ src/test/java/com/javatechie/spring/mockito/api/SpringBootMockitoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.javatechie.spring.mockito.api;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -7,6 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootMockitoApplicationTests {
-
+    @Test
+	public void contextLoads() {
+	}
 	
 }
